ff933fadfbd67b61359cb9f642d4470e0e484032
Shanu Dey <sd4shanudey@gmail.com>
2022-01-05 19:23:10 +0530

    added new todo from input text box
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import TodoList from "./TodoList";
 
 function App() {
   const [todos, setTodos] = useState(["Todo 1", "Todo 2"]);
+  const inputTodoRef = useRef("");
+
+  function handleAddTodo() {
+    const newTodo = inputTodoRef.current.value;
+    setTodos([...todos, newTodo]);
+  }
 
   return (
     <>
-      <input type="text" />
-      <button>Add Todo</button>
+      <input type="text" ref={inputTodoRef} />
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Todo</button>
       <TodoList todos={todos} />
     </>
