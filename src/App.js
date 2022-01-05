165c87bfe64226a68bb1194ede55e20266ec6792
Shanu Dey <sd4shanudey@gmail.com>
2022-01-05 19:15:23 +0530

    Ui created
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React, { useState } from "react";
+import TodoList from "./TodoList";
+
 function App() {
+  const [todos, setTodos] = useState(["Todo 1", "Todo 2"]);
+
   return (
-    null
+    <>
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Todo</button>
+      <TodoList todos={todos} />
+    </>
   );
 }
 
---
+++ src/TodoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Todo from "./Todo";
+
+export default function TodoList({ todos }) {
+  return (
+    <div>
+      <h3> Todo List: </h3>
+      {todos.map((todo) => {
+        return <Todo todo={todo} />;
+      })}
+    </div>
+  );
+}
