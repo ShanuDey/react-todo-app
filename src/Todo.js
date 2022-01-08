15b77995db12c9003bb33330a6fd0576c419895f
Shanu Dey <sd4shanudey@gmail.com>
2022-01-08 09:48:42 +0530

    improved todo list design
    Signed-off-by: Shanu Dey <sd4shanudey@gmail.com>

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { ListGroup } from "react-bootstrap";
 
 export default function Todo({ todo, handleCheckboxStateChange }) {
   const [checkState, setCheckState] = useState(false);
@@ -8,16 +9,18 @@ export default function Todo({ todo, handleCheckboxStateChange }) {
   }, [checkState]);
 
   return (
-    <div className="form-check">
-      <input
-        className="form-check-input"
-        type="checkbox"
-        checked={todo.COMPLETED}
-        onChange={(e) => setCheckState(e.target.value)}
-      />
-      <label className="form-check-label" htmlFor="flexCheckChecked">
-        {todo.NAME}
-      </label>
-    </div>
+    <ListGroup.Item>
+      <div className="form-check">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          checked={todo.COMPLETED}
+          onChange={(e) => setCheckState(e.target.value)}
+        />
+        <label className="form-check-label" htmlFor="flexCheckChecked">
+          {todo.NAME}
+        </label>
+      </div>
+    </ListGroup.Item>
   );
 }
